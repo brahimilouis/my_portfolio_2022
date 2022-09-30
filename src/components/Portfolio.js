1d81cb3f374578f128bb248a6720d2dcef1bb4d7
Brahimi <louis.brahimi@epitech.eu>
2022-09-30 17:57:01 +0200

    first

---
+++ src/components/Portfolio.js
@@ -0,0 +1,15 @@
+import '../styles/Porfolio.scss';
+import '../styles/reset.scss';
+import Navigation from "./navigations/Navigation";
+
+function Portfolio() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation/>
+      </header>
+    </div>
+  );
+}
+
+export default Portfolio;
